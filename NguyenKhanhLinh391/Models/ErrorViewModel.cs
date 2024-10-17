7e8ebe4e300792399c1751dc5e305882ebb7e885
Nguyễn Khánh Linh <nklinh021502@gmail.com>
2024-10-18 01:33:11 +0700

    Create_Project

---
+++ NguyenKhanhLinh391/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenKhanhLinh391.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
